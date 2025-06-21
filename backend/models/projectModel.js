0d194fc7636de43250f1918b8c73d6bf0e808373
SagnikGos <sagnik.gos06@gmail.com>
2025-06-21 11:59:14 +0530

    updated routes

--- backend/models/projectModel.js
+++ backend/models/projectModel.js
@@ -1,21 +1,40 @@
 import mongoose from 'mongoose';
 
 const nodeSchema = new mongoose.Schema({
-  id: { type: String, required: true }, // React Flow node ID
+  id: { type: String, required: true },
   data: {
-    label: { type: String, required: true }, // The main content/text of the node
-    prompt: String, // The user prompt that generated this node
+    label: { type: String, required: true },
+    prompt: String,
   },
   position: {
     x: { type: Number, required: true },
     y: { type: Number, required: true },
   },
+  title: { type: String, required: false },
 });
 
 const edgeSchema = new mongoose.Schema({
-  id: { type: String, required: true }, // React Flow edge ID
-  source: { type: String, required: true }, // Source node ID
-  target: { type: String, required: true }, // Target node ID
+  id: { type: String, required: true },
+  source: { type: String, required: true },
+  target: { type: String, required: true },
+});
+
+
+const opportunitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  type: { type: String, required: true },
+  market: { type: String, required: true },
+  target: { type: String, required: true },
+  main_competitors: { type: String, required: true },
+  trendAnalysis: { type: String, required: true },
+});
+
+const projectRatingSchema = new mongoose.Schema({
+  opportunity: { type: Number, required: true },
+  problem: { type: Number, required: true },
+  feasibility: { type: Number, required: true }, 
+  why_now: { type: Number, required: true },
+  feedback: { type: String, required: false },
 });
 
 const projectSchema = new mongoose.Schema({
@@ -31,6 +50,12 @@ const projectSchema = new mongoose.Schema({
   },
   nodes: [nodeSchema],
   edges: [edgeSchema],
+
+  opportunity: { type: opportunitySchema, required: false }, 
+  projectRating: {
+    type: projectRatingSchema,
+    required: false,
+  },
 }, { timestamps: true });
 
 const Project = mongoose.model('Project', projectSchema);
